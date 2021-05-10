016fee0e7d1cd5ec427129722c8273e37b75af92
Stu Behan <33100049+StuBehan@users.noreply.github.com>
2021-05-10 19:43:57 +0100

    updated app.js to remove errors and add comments

--- app.js
+++ app.js
@@ -1,36 +1,36 @@
+// Set enviroment if not production, uses config from .env file
 if (process.env.NODE_ENV !== 'production') {
   require('dotenv').config()
 }
 
+// Pull in express.js
 const express = require('express');
+
+// Pull in the db connection
 const connection = require('./config/db');
-var cors = require('cors');
-
-// routes
-const games = require('./routes/api/games');
-
 const app = express();
 
-// Connect Database
+// Pull in cors
+var cors = require('cors');
+app.use(cors({ origin: true, credentials: true }));
+
+// Pull in all routes
+const games = require('./routes/api/games');
+
+// Start the database connection if the env is not test
 if (process.env.NODE_ENV !== 'test') {
   connection.dbconnect();
 }
 
+// Init Middleware, this allows parsing of incoming payloads that contain JSON
+app.use(express.json());
 
-// cors
-app.use(cors({ origin: true, credentials: true }));
-
-// Init Middleware
-app.use(express.json({ extended: false }));
-
-app.get('/', (req, res) => res.send('Hello world!'));
-
-
-// use routes
+// Sets the url for the routes contained within the second arg
 app.use('/api/games', games);
 
+// set listening port from .env or 8082 if not present
 const port = process.env.PORT || 8082;
-
+// Listen to the port for requests
 app.listen(port, () => console.log(`Server running on port ${port}`));
 
 module.exports = app;
